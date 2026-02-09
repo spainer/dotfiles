246ded084876f73c3c27951041fdf68ddfb89e91
Sven Painer <sven.painer@viakom.de>
2026-02-09 08:35:37 +0100

    removed sketchybar

--- dot_config/sketchybar/items/executable_clock.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-config=(
-    update_freq=10
-    script="$PLUGIN_DIR/clock.sh"
-    background.padding_left=$RADIUS
-    background.padding_right=$RADIUS
-)
-sketchybar --add item clock right --set clock "${config[@]}"
